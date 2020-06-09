b3bb20a38996131d88e4d199117cc12fbfdbde7d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-06-09 19:51:42 +0200

    Write exercise for use cookie for session

---
+++ src/main/java/ProfileServlet.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+
+        request.getRequestDispatcher("link.html").include(request, response);
+
+        Cookie[] cookies =request.getCookies();
+        boolean wasFoundCookie = false;
+        for(Cookie singleCookie: cookies){
+            if(singleCookie.getName().equals("name")){
+                String name=singleCookie.getValue();
+                if(name!=null||!name.equals("")){
+                    out.print("<b>Welcome to Profile</b>");
+                    out.print("<br>Welcome, "+name);
+                    wasFoundCookie = true;
+                }
+            }
+        }
+        if(!wasFoundCookie){
+            out.print("Please login first");
+            request.getRequestDispatcher("login.html").include(request, response);
+        }
+        out.close();
+    }
+}  
